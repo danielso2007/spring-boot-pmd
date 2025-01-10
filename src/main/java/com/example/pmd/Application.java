3000eb3f34d2fc9bffb5718f7861abaa6b1998e1
Daniel Oliveira <danielso2007@gmail.com>
2025-01-10 19:48:25 -0300

    feat: Novas regras do PMD para a aplicação.

--- src/main/java/com/example/pmd/Application.java
+++ src/main/java/com/example/pmd/Application.java
@@ -3,10 +3,22 @@ package com.example.pmd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principal da aplicação.
+ */
 @SpringBootApplication
-public class Application {
+public final class Application {
 
-    public static void main(String[] args) {
+    /**
+     * Construtor padrão.
+     */
+    private Application() {}
+
+    /**
+     * Método principal do Spring-boot.
+     * @param args Argumentos de linha de comando.
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/com/example/pmd/controller/HelloController.java
+++ src/main/java/com/example/pmd/controller/HelloController.java
@@ -3,9 +3,17 @@ package com.example.pmd.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Classe de controle da aplicação.
+ */
+@SuppressWarnings("PMD.AtLeastOneConstructor")
 @RestController
 public class HelloController {
 
+    /**
+     * Método de teste rest local.
+     * @return Uma string de saudação.
+     */
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
